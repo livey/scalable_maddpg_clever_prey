45ba465e3e54d31b01da5ccdca72c3f5f9405575
Fuwei Li <liveysmile@gmail.com>
2017-12-14 18:59:28 -0800

    Update slow_replay.py
    use action instead of noise action, delete summary, delete preceive

--- slow_replay.py
+++ slow_replay.py
@@ -23,19 +23,16 @@ for episode in range(max_episode):
     agents_state, prey_state = Env.reset()
 
     for epoch in range(max_epoch):
-        agents_action = maddpg.noise_action(agents_state)
+        agents_action = maddpg.action(agents_state)
         prey_action = np.random.uniform(-1, 1)
 
         agents_next_state, prey_next_state, agents_reward, prey_reward, done = Env.step(agents_action, prey_action)
         # pause to show the image
         time.sleep(.2)
-
-        maddpg.perceive(agents_state, agents_action, agents_reward, agents_next_state,done)
         agents_state = agents_next_state
         if done:
             print('Done!!!!!!!!!!!! at epoch{} , reward:{}'.format(epoch,agents_reward))
             # add summary for each episode
-            maddpg.summary(episode)
             break
     if epoch ==max_epoch-1:
         print('Time up >>>>>>>>>>>>>>')
